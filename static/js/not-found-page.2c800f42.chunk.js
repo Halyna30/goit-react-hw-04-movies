4cdfd09ff49a28901ef67fd8af6df7eae507173c
Halyna Pryn <prynhalyna@gmail.com>
2021-03-18 01:46:42 +0100

    Updates

---
+++ static/js/not-found-page.2c800f42.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[5],{67:function(t,e,i){"use strict";i.r(e);var n=i(1);e.default=function(){return Object(n.jsx)("h1",{children:"404 - page not found"})}}}]);
+//# sourceMappingURL=not-found-page.2c800f42.chunk.js.map
